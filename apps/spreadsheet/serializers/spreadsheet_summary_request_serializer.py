752eb70800f06675ac8b4e96cb3afa9a50b06014
Michał Domino <michal.domino@yahoo.com>
2021-09-25 11:03:43 +0200

    Add set start_row default value in SpreadsheetSummaryRequestSerializer

--- apps/spreadsheet/serializers/spreadsheet_summary_request_serializer.py
+++ apps/spreadsheet/serializers/spreadsheet_summary_request_serializer.py
@@ -3,5 +3,5 @@ from rest_framework import serializers
 
 class SpreadsheetSummaryRequestSerializer(serializers.Serializer):
     file_name = serializers.CharField()
-    start_row = serializers.IntegerField()
+    start_row = serializers.IntegerField(default=0)
     columns = serializers.ListField(child=serializers.CharField())
